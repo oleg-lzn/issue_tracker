feff2bd24c7b6376677ea3a3072d5c48364f1adb
Oleg <luzeninoleg2012@gmail.com>
2025-10-12 13:50:58 +0300

    added Swagger

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,14 +2,27 @@ import { NextRequest, NextResponse } from 'next/server'
 import { headers, cookies } from 'next/headers'
 
 export const GET = (req: NextRequest) => {
+  /**
+   * @swagger
+   * /api/hello:
+   *   get:
+   *     description: Returns a hello world message
+   *     responses:
+   *       200:
+   *         description: Hello World response
+   */
   return NextResponse.json({ data: { message: 'Hello, World!' } })
 }
 
-export const POST = async (
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) => {
-  const data = await req.json()
-  const authKey = req.headers.get('authorization')
-  return NextResponse.json(data)
+export async function POST(request: Request) {
+  const data = await request.json()
+  // go and create the thing in the db
+  console.log('Received data:', data)
+  return NextResponse.json(
+    {
+      message: 'Thing created successfully',
+      thing: data,
+    },
+    { status: 201 }
+  )
 }
